6fbd56064a84db09793603baf13f9d118431b4ad
Dmitry Goncharenko <dgonch@microsoft.com>
2020-03-09 22:09:18 -0700

    added new files

---
+++ omnisharp/plugin/PathComparer.cs
@@ -0,0 +1,40 @@
+﻿// See https://github.com/microsoft/MSBuildPrediction/blob/4df4c90e0ef0e799c4f2d9c2b79c582dbc907050/src/BuildPrediction/PathComparer.cs
+
+namespace OmniSharp.FastCodeNavPlugin
+{
+    using System;
+
+    /// <summary>
+    /// Gets the appdomain-wide appropriate filesystem path string comparer
+    /// appropriate to the current operating system.
+    /// </summary>
+    internal static class PathComparer
+    {
+        public static readonly StringComparer Instance = GetPathComparer();
+        public static readonly StringComparison Comparison = GetPathComparison();
+
+        private static StringComparer GetPathComparer()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                case PlatformID.Unix:
+                    return StringComparer.Ordinal;
+                default:
+                    return StringComparer.OrdinalIgnoreCase;
+            }
+        }
+
+        private static StringComparison GetPathComparison()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                case PlatformID.Unix:
+                    return StringComparison.Ordinal;
+                default:
+                    return StringComparison.OrdinalIgnoreCase;
+            }
+        }
+    }
+}
---
+++ omnisharp/plugin/SourceBlockExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace OmniSharp.FastCodeNavPlugin
+{
+    public static class SourceBlockExtensions
+    {
+        public static IList<T> ReceiveAll<T>(this IReceivableSourceBlock<T> buffer)
+        {
+            IList<T> receivedItems = new List<T>();
+            T receivedItem = default(T);
+            while (buffer.TryReceive(out receivedItem))
+            {
+                receivedItems.Add(receivedItem);
+            }
+            return receivedItems;
+        }
+    }
+}
